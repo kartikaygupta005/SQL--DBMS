e88c963898c008a3a16df8500941ebb032eeb1ea
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-09-04 18:56:51 +0530

    Create DQL.sql

---
+++ DQL.sql
@@ -0,0 +1,26 @@
+-- DQL — Data Query Language
+
+-- DQL is used to retrieve data.
+
+-- Main command:
+
+-- SELECT
+
+-- Example:
+
+SELECT * FROM Employee;
+
+-- This displays the data.
+
+-- You can also select specific columns:
+
+SELECT name, salary
+FROM Employee;
+
+-- Output:
+
+-- name   | salary
+-- ---------------
+-- Rahul  | 60000
+-- Amit   | 50000
+-- Raj    | 70000
